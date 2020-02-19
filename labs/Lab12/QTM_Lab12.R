dcffc0ace6230243ec64517acdc7689dc93668ca
jinahyejin <59975110+jinahyejin@users.noreply.github.com>
2020-02-19 15:46:20 -0500

    lab 12 work

--- labs/Lab12/QTM_Lab12.R
+++ labs/Lab12/QTM_Lab12.R
@@ -45,7 +45,7 @@ colnames(x)
 # 1. Run a linear model with papprove as a dependent variable and 
 #    Post, osu, and the interaction of the two as independent variables.
 
-
+lm(papprove ~ Post + osu + Post:osu, data = x)
 
 
 # 2. Answer the following questions based on the results.
@@ -53,18 +53,33 @@ colnames(x)
 # 2a. What is the predicted presidential approval of OSU students who received
 #     the survey BEFORE the game?
 
+4.5619 + (-0.3780)
+4.1839
 
 # 2b. What is the predicted presidential approval of OSU students who received
 #     the survey AFTER the game?
 
 
+4.5619 - 0.4469 - 0.3780 + 0.8960
+4.633
+
 # 2c. What is the predicted presidential approval of UO students who received
 #     the survey BEFORE the game?
+
+4.5619
 
 
 # 2d. What is the predicted presidential approval of UO students who received
 #     the survey AFTER the game?
 
+4.5619 -0.4469 
+4.115
 
 # 3e. What is the marginal effect of Post on presidential approval
 #     when osu=1?
+
+post <- 4.5619 - 0.4469 -0.3780 + 0.8960
+pre <- 4.5619 - 0.3780
+post-pre = -1.3429
+
+#marginal effect of pre to post in osu students (difference)
