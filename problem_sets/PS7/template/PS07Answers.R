23627dbde4f46dc4bee0a2c8cc1ab8d4ceae16a0
jinahyejin <59975110+jinahyejin@users.noreply.github.com>
2020-05-06 20:48:10 -0400

    answer of ps7

--- problem_sets/PS7/template/PS07Answers.R
+++ problem_sets/PS7/template/PS07Answers.R
@@ -38,16 +38,33 @@ library("lme4")
 library("ggplot2")
 library("googleVis")
 library("sjPlot")
-
+library("Matrix")
 #####################
 # Problem 1
 #####################
 
+mexico_elections <- read_csv("GitHub/QTM200Spring2020/problem_sets/PS7/MexicoMuniData.csv")
+mexicomodel <- glm(PAN.visits.06 ~ competitive.district + marginality.06 + PAN.governor.06, data = mexico_elections, family = poisson)
+summary(mexicomodel)
+coefficients(mexicomodel)
 
-mexico_elections <- read.csv("MexicoMuniData.csv", stringsAsFactors = F, header=T)
+estimate <- exp(-3.9304467 + -0.4594186+ -0.2073147)
+estimate
 
 #####################
-# Problem 1
+# Problem 2
 #####################
 
 sleepstudy <- sleepstudy
+pooling1 <- lm(Reaction ~ Days, data = sleepstudy)
+summary(pooling1)
+pooling2 <- lm(Reaction ~ Days + Subject -1, data = sleepstudy)
+fitted.values(pooling2)
+pooling3 <- lm(Reaction ~ Days * Subject, data = sleepstudy)
+fitted.values(pooling3)
+summary(pooling3)
+pooling3predict <- lm(Reaction ~ (Days|Subject), data = sleepstudy)
+pooling4 <- lm(Reaction ~ Days + Subject + Days:Subject, data = sleepstudy)
+fitted.values(pooling4)
+pooling5 <- lmer(Reaction ~ Days, data = sleepstudy)
+summary(pooling5)
